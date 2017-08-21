42b75764c334b1b814598b4fc8d9a5ab57848bd1
elena-step <kashyx53@ukr.net>
2017-08-21 14:38:37 +0300

    "Deposit" is located in a new branch

---
+++ src/javasummer2017/Deposit.java
@@ -0,0 +1,118 @@
+package javasummer2017;
+
+import java.util.Scanner;
+
+public class Deposit {
+
+    public static void main(String[] args) {
+
+        System.out.println("Please, input data. ");
+        System.out.print("Deposit, USD: ");
+        double bodyDepositUser = inputDataUser();
+
+        System.out.print("Persent, USD: ");
+        double persentUser = inputDataUser();
+
+        System.out.print("Amount of monthly interest, USD: ");
+        double monthlyInterestUser = inputDataUser();
+
+        System.out.print("Amount deposit with interest, USD: ");
+        double depositWithInterestUser = inputDataUser();
+
+        System.out.println();
+        System.out.println("Data inputed: ");
+        System.out.print("Deposit, USD: ");
+        printData(bodyDepositUser);
+        System.out.print("Persent, USD: ");
+        printData(persentUser);
+        System.out.print("Amount of monthly interest, USD: ");
+        printData(monthlyInterestUser);
+        System.out.print("Amount deposit with interest, USD: ");
+        printData(depositWithInterestUser);
+
+        int monthFromDepositOpening_ForExceedingInterest = numberOfMonthExceedingInterest(bodyDepositUser,
+                persentUser, monthlyInterestUser);
+        System.out.println();
+        System.out.printf("The value of the monthly interest will exceed %.2f USD on the %d th month " +
+                "from the deposit opening.", monthlyInterestUser, monthFromDepositOpening_ForExceedingInterest);
+
+        int monthFromDepositOpening_ForExceedingDeposit = numberOfMonthExceedingDeposit(bodyDepositUser,
+                persentUser, depositWithInterestUser);
+        System.out.println();
+        System.out.printf("The value of the deposit with interest will exceed %.2f USD in %d months " +
+                "from the deposit opening.", depositWithInterestUser, monthFromDepositOpening_ForExceedingDeposit);
+        //test();
+    }
+
+    private static double inputDataUser() {
+        Scanner scan = new Scanner(System.in);
+        double data = 0;
+        if (!scan.hasNextDouble()) {
+            System.err.println("Input error!");
+            System.exit(0);
+        } else {
+            data = scan.nextDouble();
+        }
+        return data;
+    }
+
+    private static void printData(double data) {
+        System.out.printf("%.2f", data);
+        System.out.println();
+    }
+
+
+    private static int numberOfMonthExceedingInterest(double deposit, double persent, double monthlyInterestUser) {
+        int month = 0;
+        double monthlyInterest = 0;
+        while (monthlyInterest < monthlyInterestUser) {
+            monthlyInterest = deposit * persent / 100;
+            deposit += monthlyInterest;
+            month++;
+        }
+        return month;
+    }
+
+    private static int numberOfMonthExceedingDeposit(double deposit, double persent, double depositWithInterestUser) {
+        int month = 0;
+        double depositWithInterest = deposit;
+        while (depositWithInterest < depositWithInterestUser) {
+            depositWithInterest += depositWithInterest * persent / 100;
+            month++;
+        }
+        return month;
+    }
+
+    private static void test() {
+        double bodyDeposit = 1000.00;
+        double persent = 2.00;
+        double monthlyInterest = 30.00;
+        double depositWithInterest = 1200.00;
+
+        int expectedMonth_ForExceedingInterest = 22;
+        int expectedMonth_ForExceedingDeposit = 10;
+
+        int month_ForExceedingInterest = numberOfMonthExceedingInterest(bodyDeposit, persent, monthlyInterest);
+        int month_ForExceedingDeposit = numberOfMonthExceedingDeposit(bodyDeposit, persent, depositWithInterest);
+
+        boolean passed = true;
+        if (month_ForExceedingInterest != expectedMonth_ForExceedingInterest) {
+            System.out.printf("Test error: month (exceeding interest) = %d, but expected = %d .",
+                    month_ForExceedingInterest, expectedMonth_ForExceedingInterest);
+            System.out.println();
+            passed = false;
+        }
+
+        if (month_ForExceedingDeposit != expectedMonth_ForExceedingDeposit) {
+            System.out.printf("Test error: month (exceeding deposit) = %d, but expected = %d .",
+                    month_ForExceedingDeposit, expectedMonth_ForExceedingDeposit);
+            System.out.println();
+            passed = false;
+        }
+
+        if (passed) {
+            System.out.println("Test passed.");
+        }
+    }
+
+}
